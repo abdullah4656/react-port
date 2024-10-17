a3b5cd8cd7bbf85082c00c8754641fc08c9036ad
Abdullah Yasin <abdullahyasin4656@gmail.com>
2024-10-17 11:24:36 +0500

    first commit

--- src/componenets/Into.jsx
+++ src/componenets/Into.jsx
@@ -1,54 +1,49 @@
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
 
-
-const Into = () => {
+const Intro = () => {
   return (
-  <>
+    <>
+      <div className='intro-cont p-2 mq-main-form'>
+        <div className='p-8'>
+          <h2>
+            <strong className='font-serif text-6xl text-blue-400 italic logo'>
+              Code Viper
+            </strong>
+          </h2>
+          <h3 className='flex justify-center items-center text-4xl intro-desc'>
+            <TypeAnimation
+              sequence={['Leading Software Solutions Company', 2000]}
+              wrapper="span"
+              speed={60}
+              className='text-blue-800 font-arial font-semibold'
+              repeat={0}
+            />
+          </h3>
+          <br />
+          <div className="flex flex-col items-center justify-center">
+            <p className="
+              md:text-xl
+              lg:text-2xl
+              text-gray-800
+              flex justify-center items-center mx-auto
+            ">
+              Transform your business with custom IT solutions designed to meet specific needs. Expertise ensures competitiveness and success through innovative technology and digital strategies.
+            </p>
+          </div>
+        </div>
+        <img className='w-1/4 mq-main-img rounded-xl bg-blue-400' src="images/home-2.png" alt="home visual" />
+      </div>
 
-  <div className='introcont p-2 mq-main-form'>
-  <div className='p-8' >
- 
-      <h2>
-        <strong className='font-serif text-6xl text-blue-400 name italic logo     '>Code Viper</strong>
-      </h2>
-      <h3 className='flex  justify-center items-center text-4xl intro-desc '>
-      <TypeAnimation
-      sequence={[
-        
-        'Leading Software Solutions Company',
-        2000,
-        
-      ]}
-      wrapper="span"
-      speed={60}
-      style={{ fontFamily:'none'}}
-      className='text-blue-800   font-arial font-semibold'
-      repeat={0}
-    />
-       
-      </h3>  
-      <br />
-      <div className="flex flex-col items-center justify-center  ">
-      <p className="
-        
-         md:text-xl
-          lg:text-2xl 
-            text-gray-800
-            flex justify-center items-center mx-auto
-      ">
-      Transform your business with custom IT solutions designed to meet specific needs. Expertise ensures competitiveness and success through innovative technology and digital strategies.</p>
-    </div>
-  
-     
-    </div>
-  
-    <img className='w-1/4 mq-main-img rounded-xl  bg-blue-400 ' src="images/home-2.png" alt="" />
-  
-   
-</div>
+      {/* CSS style for logo */}
+      <style jsx>{`
+        .logo {
+          font-family: "Audiowide", sans-serif;
+          font-size: 50px;
+        }
+      `}</style>
     </>
   );
 };
 
-export default Into;
+export default Intro;
