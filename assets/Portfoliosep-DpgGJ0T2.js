47071d9f4eebe2666777e8400f7e7e0e6bb9a1a2
Abdullah Yasin <abdullahyasin4656@gmail.com>
2024-10-19 14:04:10 +0500

    Updates

--- assets/Portfoliosep-DpgGJ0T2.js
+++
@@ -1,7 +0,0 @@
-import{j as s}from"./index-C7BATJEM.js";import{N as e,F as a}from"./Footer-WMXEbcg0.js";const i=()=>s.jsxs(s.Fragment,{children:[s.jsx(e,{}),s.jsxs("section",{className:"w-full  psec",children:[s.jsx("h2",{className:"flex items-center  justify-center text-5xl font-extrabold m-10",style:{color:"#404040"},children:"Our Portfolio"}),s.jsxs("div",{className:"flex gap-10 p-5 justify-evenly flex-wrap pb-40",children:[s.jsx("a",{href:"",className:"none",children:s.jsx("img",{className:"",src:"images/Buses_Plus_1.png",alt:""})}),s.jsx("a",{href:"",children:s.jsx("img",{className:"w-1/4",src:"images/sponge_3.png",alt:""})}),s.jsx("a",{href:"",children:s.jsx("img",{className:"w-1/4",src:"images/Watermart.png",alt:""})}),s.jsx("a",{href:"",children:s.jsx("img",{className:"w-1/4",src:"images/Thorai.jpeg",alt:""})}),s.jsx("a",{href:"",children:s.jsx("img",{className:"w-1/4",src:"images/wise_eye_1.png",alt:""})}),s.jsx("a",{href:"",children:s.jsx("img",{className:"w-1/4",src:"images/Buses_Plus_1.png",alt:""})})]})]}),s.jsx(a,{}),s.jsx("style",{jsx:!0,children:`
-    .psec img{
-      height: 100px;
-          width: 300px;
-    
-    }
-    `})]});export{i as default};
